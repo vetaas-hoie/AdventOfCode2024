78b185895f7b7cde431f9e214d2b3b00770f8503
Asbjørn Vetaas-Høie <asbjorn@vetaas-hoie.com>
2024-12-01 15:34:38 +0100

    DAy 1

--- Program.cs
+++ Program.cs
@@ -19,10 +19,53 @@ namespace AdventOfCode
             string[] a = (string[])File.ReadAllLines(Directory.GetCurrentDirectory() + "\\..\\..\\..\\text.txt");
             Console.WriteLine("Total lines: " + a.Length);
 
+
+            List<int> list1 = parse(a, 1);
+            List<int> list2 = parse(a, 2);
+            //Sorterer
+            list1.Sort();
+            list2.Sort();
+
+            //Del 1---------------------------------------------------------------------------------------
+            int distance = 0;
+            for (int i = 0; i < list1.Count; i++) { 
+                distance += Math.Abs(list1[i] - list2[i]);
+                    }
+            Console.WriteLine("Total distance: " + distance);
+
+            //Del 2---------------------------------------------------------------------------------------
+            int similarityScore = 0;
+            foreach (int i in list1) {
+                similarityScore += i * list2.FindAll(c => c.Equals(i)).Count();
+            }
+            Console.WriteLine("Similarity Score: " + similarityScore);
+          
             Console.ReadKey();
+           
+            
         }
 
+        static List<int> parse(string[] list, int col)
+        {
+            List<int> ints = new List<int>();
+            foreach (string i in list) {
+                int a = 0;
+                switch (col)
+                {
+                    case <= 1:
+                     a = Int32.Parse(i.Split(' ').First());
+                     break;
 
+                    case > 1:
+                        a = Int32.Parse(i.Split(' ').Last());
+                        break;
+                }
+                ints.Add(a);
+                           
+            }
+            return ints;
+
+        }
 
 
     }
