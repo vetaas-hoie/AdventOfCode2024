f21ba0d2e6b6951e7a2e7aac3398cd6f881245c4
Asbjørn Vetaas-Høie <asbjorn.vetaas-hoie@capgemini.com>
2024-12-01 12:39:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Reading File: ");
+            //string[] a = (string[])File.ReadAllLines("C:\\Users\\avetaash\\source\\repos\\AdventOfCode2024\\text.txt");
+            string[] a = (string[])File.ReadAllLines(Directory.GetCurrentDirectory() + "\\..\\..\\..\\text.txt");
+            Console.WriteLine("Total lines: " + a.Length);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
