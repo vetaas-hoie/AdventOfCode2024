b80c5a098f6d126460b64a01386e9503290e4fb0
Asbjørn Vetaas-Høie <asbjorn@vetaas-hoie.com>
2024-12-03 00:12:55 +0100

    Day 2 Finished

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 
 namespace AdventOfCode
 {
@@ -13,15 +9,75 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Reading File: ");
-            //string[] a = (string[])File.ReadAllLines("C:\\Users\\avetaash\\source\\repos\\AdventOfCode2024\\text.txt");
-            string[] a = (string[])File.ReadAllLines(Directory.GetCurrentDirectory() + "\\..\\..\\..\\text.txt");
-            Console.WriteLine("Total lines: " + a.Length);
+            string[] input = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\..\\..\\..\\text.txt");
+            Console.WriteLine("Total lines: " + input.Length);
+            List<List<int>> reports = input.Select(ParseString).ToList();
 
+            int safeReports = 0, safeReportPart2=0;
+
+            foreach (var report in reports)
+            {
+                if (IsRising(report) || IsFalling(report))
+                {
+                    safeReports++;
+                    safeReportPart2++;
+                }
+                else if (CanBeSafeWithOneRemoval(report))
+                {
+                    safeReportPart2++;
+                }
+            }
+
+            Console.WriteLine("Safe Reports: " + safeReports);
+            Console.WriteLine("Safe Reports with dampener: " + safeReportPart2);
             Console.ReadKey();
         }
 
+        private static bool IsRising(List<int> levels)
+        {
+            for (int i = 1; i < levels.Count; i++)
+            {
+                if (levels[i - 1] >= levels[i] || Math.Abs(levels[i - 1] - levels[i]) < 1 || Math.Abs(levels[i - 1] - levels[i]) > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+        private static bool IsFalling(List<int> levels)
+        {
+            for (int i = 1; i < levels.Count; i++)
+            {
+                if (levels[i - 1] <= levels[i] || Math.Abs(levels[i - 1] - levels[i]) < 1 || Math.Abs(levels[i - 1] - levels[i]) > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool CanBeSafeWithOneRemoval(List<int> levels)
+        {
+            for (int i = 0; i < levels.Count; i++)
+            {
+                
+                var modifiedLevels = new List<int>(levels);
+                modifiedLevels.RemoveAt(i);
+
+                
+                if (IsRising(modifiedLevels) || IsFalling(modifiedLevels))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static List<int> ParseString(string s)
+        {
+            return s.Split(' ').Select(int.Parse).ToList();
+        }
     }
 }
